2c7c5d1bd45777e83e9e0a4039ca38ea25ca128f
Lucas <lucas_cleminson@eticalgarve.com>
2026-01-29 21:53:06 +0000

    add: added theme toggle

--- stone_saints/app/layout.tsx
+++ stone_saints/app/layout.tsx
@@ -1,3 +1,5 @@
+import { ThemeProvider } from "@/components/theme-provider"
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -23,11 +25,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" suppressHydrationWarning>
+      <head  />
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
         {children}
+        </ThemeProvider>
       </body>
     </html>
   );
---
+++ stone_saints/components/theme-toggle.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import * as React from "react";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+import { Sun, Moon } from "lucide-react";
+
+export default function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <Button
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      variant="outline"
+    >
+      {theme === "dark" ? <Sun /> : <Moon />}
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
